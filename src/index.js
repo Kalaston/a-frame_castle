3a0ea334e88f093c755dc01d9b3a490e7fad9002
William Diaz <wdiazux@gmail.com>
2018-10-14 21:34:18 -0600

    fixing service worker

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ require('./components/yolistli-portal');
 
 window.addEventListener('DOMContentLoaded', () => {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./service-worker.js').then(registration => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
             console.log('SW registered: ', registration);
         }).catch(registrationError => {
             console.log('SW registration failed: ', registrationError);
